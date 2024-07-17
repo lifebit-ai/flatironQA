124643b72240f261696b90045d0d84ce3ca1afe9
hamzah-lifebit <110822354+hamzah-lifebit@users.noreply.github.com>
2024-07-17 10:45:20 +0100

    Delete supporting_scripts directory

--- supporting_scripts/report.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cp -r ${1}/* .
-
-Rscript -e "rmarkdown::render(
-    'report.Rmd',
-    params = list(demographics='/data/${demographics}', mortality='/data/${mortality}', diagnosis='/data/${diagnosis}', date_var='${date_var}', group_var='${group_var}', market='${market}', tumor='${tumor}', delivery='${delivery}')
-)"
-mv report.html multiqc_report.html
